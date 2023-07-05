02b201561f89f61f7a2c685d0322dbfe620fcd24
SalvadorCardona <hebustha@gmail.com>
2023-07-05 15:46:56 +0200

    WIP

--- src/Module/Shared/Component/LayoutComponent.tsx
+++ src/Module/Shared/Component/LayoutComponent.tsx
@@ -2,10 +2,18 @@ import { Outlet } from "react-router"
 import { TopMenuComponent } from "@/Module/Shared/Component/TopMenuComponent.tsx"
 
 export function LayoutComponent() {
+  const height = window.screen.height
+  const width = window.screen.width
+
+  const style = {
+    height: height,
+    width: width,
+  }
+
   return (
     <>
       <TopMenuComponent></TopMenuComponent>
-      <div className={"min-h-screen min-w-full"}>
+      <div style={style}>
         <Outlet />
       </div>
     </>
